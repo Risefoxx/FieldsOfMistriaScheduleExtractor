a747af24dc9cde1f5a4d06be4a9388cb1583e02f
risefoxx <235612456+Risefoxx@users.noreply.github.com>
2026-09-13 16:56:05 +1000

    improved formatting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tomllib
 import json
 
+
 def parse_toml(filename, schedules):
     with open(filename, "rb") as f:
         data = tomllib.load(f)
@@ -8,7 +9,7 @@ def parse_toml(filename, schedules):
     day = None
     weather = None
 
-    # get day and weather
+    # Get the day and weather from "requires"
     for requirement in data["requires"]:
         if "day_of_the_week" in requirement:
             day = requirement["day_of_the_week"]
@@ -16,32 +17,36 @@ def parse_toml(filename, schedules):
         if "weather" in requirement:
             weather = requirement["weather"]
 
-    if day not in schedules:
-        schedules[day] = {}
-
-    if weather not in schedules[day]:
-        schedules[day][weather] = {}
-
-    # add npc
+    # Add each NPC
     for villager, schedule in data.items():
 
+        # Ignore non-NPC sections
         if villager in ("requires", "start_writes", "end_writes"):
             continue
 
-        schedules[day][weather][villager] = {}
+        # Create the nested dictionaries if necessary
+        if villager not in schedules:
+            schedules[villager] = {}
 
-        # add time/location
+        if day not in schedules[villager]:
+            schedules[villager][day] = {}
+
+        if weather not in schedules[villager][day]:
+            schedules[villager][day][weather] = {}
+
+        # Add each time/location
         for time, information in schedule.items():
 
-
+            # Format:
             # "6:00am" = "some/location"
             if isinstance(information, str):
-                schedules[day][weather][villager][time] = information
+                schedules[villager][day][weather][time] = information
 
+            # Format:
             # [npc."6:00am"]
             # destination = "some/location"
             else:
-                schedules[day][weather][villager][time] = information["destination"]
+                schedules[villager][day][weather][time] = information["destination"]
 
 
 schedules = {}
@@ -52,6 +57,7 @@ parse_toml("./toml_files/spring_wednesday.s.toml", schedules)
 parse_toml("./toml_files/spring_thursday.s.toml", schedules)
 parse_toml("./toml_files/spring_saturday.s.toml", schedules)
 parse_toml("./toml_files/spring_sunday.s.toml", schedules)
+
 
 with open("schedules_spring.json", "w") as f:
     json.dump(schedules, f, indent=4)
